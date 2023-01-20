ac477d03df5200c15811c825a3dd80abc7156168
skdytpq <74703237+skdytpq@users.noreply.github.com>
2023-01-20 16:29:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
    return 'This is joseph'
-
-if __name__ == '__main__':
-   app.run('0.0.0.0', port=5001, debug=True)
    
 @user_bp.route('/create', methods=['POST'])
 def create():
     params = request.get_json()
     return 'ok'
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5001, debug=True)
