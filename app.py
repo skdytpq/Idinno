56605ee847cb883ef2faecdb7f033df29487fafa
skdytpq <74703237+skdytpq@users.noreply.github.com>
2023-01-30 15:07:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ def create():
          img_id = params['pInfo'][0]['img_id']
          #os.system("curl " + img_url + " > test.jpg")
          if img_id == 'C01':
-            return 'p_of_sa_m_10_0001 '
+            return 'p_of_sa_w_20_0001,p_of_sa_w_30_0002,p_of_sa_w_40_0004'
          elif img_id == 'C02':
-            return 'p_of_sa_w_10_0001'
+            return 'p_of_sa_w_20_0002,p_of_sa_w_30_0002,p_of_sa_w_40_0003'
          elif img_id == 'C03':
-             return 'p_of_sa_w_30_0001'
+             return 'p_of_sa_w_30_0001,p_of_sa_w_30_0003,p_of_sa_w_40_0002'
          elif img_id == 'C04':
-            return 'p_of_sa_w_30_0002'
+            return 'p_of_sa_w_20_0004,p_of_sa_w_30_0005,p_of_sa_w_40_0001'
          else: 
             return 'p_of_sa_w_30_0002'
     else:
