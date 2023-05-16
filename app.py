a030b1f777ef5ae4bbe335bc271cfdcb35c8111a
Joseph Na <74703237+skdytpq@users.noreply.github.com>
2023-05-16 19:35:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def mapping(r,tp) :
          JP = td['판단/인식']
          Trend = td['트렌드\n민감도']
          Quality = td['상품/서비스품질']
-         Easy = td['이용편의성'].iloc[:,0]
+         Easy = td['이용편의성'].iloc[:,1]
          Age = int(td['age_gender'][:2])
          area_score = area_f(SN, JP, area)
          interior_score = interior_f(Trend, Easy, interior)
